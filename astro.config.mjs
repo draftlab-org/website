a30694e938f3422a9416aedd06e7a13cb91ad291
tin <tingeber@gmail.com>
2024-06-09 16:29:48 +0200

    workflow deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-import partytown from '@astrojs/partytown';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import icon from 'astro-icon';
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import partytown from '@astrojs/partytown'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astroship.web3templates.com',
+  site: 'https://draftlab.org',
   integrations: [react(), tailwind(), partytown(), mdx(), sitemap(), icon()],
-});
+})
