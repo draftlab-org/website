4cf3dc7e24d0abaaabadd0d60964a4fe503769db
tin <tingeber@gmail.com>
2024-06-09 22:29:56 +0200

    list of services, and a banana icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,13 @@ import partytown from '@astrojs/partytown'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import icon from 'astro-icon'
+import yaml from '@rollup/plugin-yaml'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://draftlab.org',
   integrations: [react(), tailwind(), partytown(), mdx(), sitemap(), icon()],
+  vite: {
+    plugins: [yaml()],
+  },
 })
